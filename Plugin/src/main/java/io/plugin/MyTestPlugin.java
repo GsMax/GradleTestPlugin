975d48ff1cef7803024879a10e95ce46e6aead1a
GsMax <gsfores@gmail.com>
2021-03-02 00:30:15 +0200

    second commit

--- Plugin/src/main/java/io/plugin/MyTestPlugin.java
+++ Plugin/src/main/java/io/plugin/MyTestPlugin.java
@@ -2,8 +2,11 @@ package io.plugin;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Dependency;
 import org.gradle.api.tasks.testing.Test;
 import org.gradle.api.tasks.testing.testng.TestNGOptions;
+
+import java.util.stream.Collectors;
 
 public class MyTestPlugin implements Plugin<Project> {
     @Override
@@ -11,6 +14,9 @@ public class MyTestPlugin implements Plugin<Project> {
         ((Test)project.getTasksByName("test", true).toArray()[0]).useTestNG();
         ((TestNGOptions)((Test)project.getTasksByName("test", true)
                 .toArray()[0]).getOptions()).getListeners().add("io.test_listener.MyTestListener");
-        project.getDependencies().add("testRuntime", "org.testng:testng:6.14.3");
+
+        project.getDependencies().add("testRuntime", "org.example:Listener:1.0-SNAPSHOT");
+//        project.getConfigurations().getByName("testRuntime").getDependencies().stream()
+//                .filter(i -> ((Dependency)i).getName().equals("Listener")).collect(Collectors.toList());
     }
 }
